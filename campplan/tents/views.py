226cfe389825a52cf8c8f6cb832235e14d8281d5
Dave Barnett <dave.barnett@jollygood.org.uk>
2022-09-22 22:18:29 +0100

    Initial views

--- campplan/tents/views.py
+++ campplan/tents/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tents.models import *
 
 def index(request):
     return HttpResponse("Hello camper.")
+
+def nations(request):
+    nations = Nation.objects.order_by("id")
+    output = "<br/>".join(["<a href='nation/%d'>%s</a>"%(n.id,n.name) for n in nations])
+    return HttpResponse(output)
+
+def nation(request, id):
+    return HttpResponse("Nation %d"%(id))
